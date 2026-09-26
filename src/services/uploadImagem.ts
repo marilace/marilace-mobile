7c02a2edcdc313d4276a6f0e1e44e2c6bb66d5f6
Emilly Brito <emillydesousabrito@gmail.com>
2026-09-26 13:43:49 -0300

    refactor(cloudinary): adapta serviço para a versão mobile

--- src/services/uploadImagem.ts
+++ src/services/uploadImagem.ts
@@ -1,17 +1,23 @@
-// export async function enviarImagem(arquivo: File): Promise<string> {
-//     const formData = new FormData()
-//     formData.append('file', arquivo)
-//     formData.append('upload_preset', import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET)
+export async function enviarImagem(uri: string): Promise<string> {
+    const formData = new FormData()
 
-//     const resposta = await fetch(
-//         `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`,
-//         { method: 'POST', body: formData }
-//     )
+    formData.append('file', {
+        uri,
+        type: 'image/jpeg',
+        name: 'upload.jpg',
+    } as any)
+    
+    formData.append('upload_preset', process.env.EXPO_PUBLIC_CLOUDINARY_UPLOAD_PRESET as string)
 
-//     if (!resposta.ok) {
-//         throw new Error('Falha ao enviar a imagem.')
-//     }
+    const resposta = await fetch(
+        `https://api.cloudinary.com/v1_1/${process.env.EXPO_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
+        { method: 'POST', body: formData }
+    )
 
-//     const dados = await resposta.json()
-//     return dados.secure_url
-// }
+    if (!resposta.ok) {
+        throw new Error('Falha ao enviar a imagem.')
+    }
+
+    const dados = await resposta.json()
+    return dados.secure_url
+}
